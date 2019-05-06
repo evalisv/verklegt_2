2e480e543f02e59801189fc429885dafa9850d6c
ThrosturSv <trosturs@gmail.com>
2019-05-06 17:22:27 +0000

    Bætti við authentication hlutanum.

--- user/views.py
+++ user/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
+
+def register(request):
+    if 'POST' == request.method:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
