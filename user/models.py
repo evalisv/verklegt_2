fcf720de7cf54530e8c880bab8d8c158bb3e4281
Sunna <sunnam17@ru.is>
2019-05-06 12:45:28 +0000

    Created apps (still emtpy) and added migration script for user.

--- user/models.py
+++ user/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     kennitala = models.IntegerField()
     phone_number = models.IntegerField()
+    address = models.CharField(max_length=255)
+    municipiality = models.CharField(max_length=255)
+    postal_code = models.IntegerField()
+    offers = models.TextField(blank=True)
+    user_roles = models.TextField()
+    messages = models.TextField(blank=True)
+    watchlist = models.TextField(blank=True)
+    estates = models.TextField(blank=True)
