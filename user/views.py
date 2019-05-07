cae0c32037f05917ebd4bcb1e855c8c8cc303979
Þröstur <trosturs@gmail.com>
2019-05-07 09:54:47 +0000

    every thing up to date

--- user/views.py
+++ user/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+from user.models import Profile
+from user.forms.profile_form import ProfileForm
+
 def index(request):
     return render(request, 'user/index.html')
+
+
+def register(request):
+    if 'POST' == request.method:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
+
+def profile(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            #tengja foreign key við innskráðan notanda
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html', {
+        'form': ProfileForm(instance=profile)
+    })
