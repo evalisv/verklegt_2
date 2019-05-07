5988a18572a31427a35dcb961456a10c3c73257e
Eva Lísa <evalisv@gmail.com>
2019-05-07 14:04:04 +0200

    Created register_estate, currently doesn't post

--- estate/views.py
+++ estate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from estate.models import Estate
+from estate.forms.estate_form import RegisterEstateForm
 # Create your views here.
 
 
@@ -10,4 +11,14 @@ def index(request):
 def get_estate_by_id(request, id):
     return render(request, 'estate/estate_details.html', {
       'estate' : get_object_or_404(Estate, pk=id)
+    })
+
+def register_estate(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        form = RegisterEstateForm()
+        # TODO: Instance new RegisterEstateForm()
+    return render(request, 'estate/register_estate.html', {
+        'form' : form
     })
