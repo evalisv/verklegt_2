8e3be780f75e2af60f081771c5661f45e4215457
Þröstur <trosturs@gmail.com>
2019-05-07 09:54:36 +0000

    every thing up to date

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     # url, fall sem á að keyrast þegar farið er á urlið, nafn á fallinu
     path('', views.index, name="user-index"),
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile', views.profile, name='profile')
 ]
