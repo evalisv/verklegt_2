c6b22bd5d1bee760de5354f667006a046805de22
Eva Lísa <evalisv@gmail.com>
2019-05-07 10:46:45 +0200

    Búin að setja upp url og ítarlegri upplýsingar með öllum myndum sem tengjast fasteign. Enn mjög hrátt

--- estate/urls.py
+++ estate/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # url, fall sem á að keyrast þegar farið er á urlið, nafn á fallinu
     path('', views.index, name="estate-index"),
+    path('<int:id>', views.get_estate_by_id)
 ]
