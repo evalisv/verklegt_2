5c83244103212df00b3ac823dcf3c09ffd0c8fd2
unknown <birtaosk93@gmail.com>
2019-05-08 12:07:31 +0200

    vantadi kommu a einum stad

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     # url, fall sem á að keyrast þegar farið er á urlið, nafn á fallinu
     path('', views.index, name="user-index"),
-    path('update_user/<int:id>', views.update_user, name="update_user")
+    path('update_user/<int:id>', views.update_user, name="update_user"),
     path('register', views.register, name='register'),
     path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
