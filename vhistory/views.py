404d710b37668e3ce3faca382920ac1fabe7bafa
Árni Fannar <arnis18@ru.is>
2019-05-09 13:26:04 +0000

    View fyrir vhistory í þróun. Sumt er að virka, en ekki allt.

--- vhistory/views.py
+++ vhistory/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from vhistory.models import Vhistory
 import datetime
+from django.utils import timezone
 
 def index(request):
-    return render(request, 'user/index.html')
+    context = {'estate': estate}
+    return render('vhistory/index.html', context)
 
-def update_vhistory(address, date):
-    today = datetime.today()
-    for estate in Vhistory.objects.all():
-        if address == estate.address:
-            estate.view_date =  today
+def update_vhistory(user, estate):
+    #today = datetime.date(datetime.today())
+    today = timezone.now()
+    for viewed_estate in Vhistory.objects.all():
+        if estate.address == viewed_estate.address:
+            viewed_estate.view_date = today
             return
-    entry = Vhistory(address=address, date=date)
+    entry = Vhistory(user=user, estate=estate, view_date=today)
     entry.save()
 
 def get_vhistory(request):
-    return render(Vhistory.objects.all().order_by('-date'), 'vhistory.html',{
+    return render(Vhistory.objects.all().order_by('-date'), 'vhistory/index.html',{
         'estate': get_object_or_404(estate,pk=id)
     })
