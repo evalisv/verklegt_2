9d9e68151742ccfca1c5ae5c22288a367e396db3
Eva Lísa <evalisv@gmail.com>
2019-05-10 10:28:57 +0200

    Added user wathclist and migrated

--- user/models.py
+++ user/models.py
@@ -9,3 +9,6 @@ class Profile(models.Model):
     address = models.CharField(max_length=255)
     postal_code = models.ForeignKey('estate.Municipality', on_delete=models.CASCADE)
 
+class WatchList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    estate = models.ForeignKey('estate.Estate', on_delete=models.CASCADE)
