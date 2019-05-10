be17b1e28b942bb22f78c34c6ed233976782d7e1
unknown <birtaosk93@gmail.com>
2019-05-10 15:42:34 +0200

    added country to user profile

--- user/forms/profile_form.py
+++ user/forms/profile_form.py
@@ -21,7 +21,7 @@ class ProfileForm(ModelForm):
             'kennitala': widgets.NumberInput(attrs={'class': 'form-control'}),
             'phone_number': widgets.NumberInput(attrs={'class': 'form-control'}),
             'address': widgets.TextInput(attrs={'class': 'form-control'}),
-            'postal_code': widgets.Select(attrs={'class': 'form-control'})
-
+            'postal_code': widgets.Select(attrs={'class': 'form-control'}),
+            'country': widgets.Select(attrs={'class': 'form-control'})
         }
 
