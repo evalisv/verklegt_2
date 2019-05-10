0efe0aab50744cdc01852c865f4a30b8701c9074
unknown <birtaosk93@gmail.com>
2019-05-10 09:05:57 +0200

    almost finished forms

--- user/forms/profile_form.py
+++ user/forms/profile_form.py
@@ -1,26 +1,25 @@
 from django.forms import ModelForm, widgets
 from django import forms
-from user.models import User
+from django.contrib.auth.models import User
 from user.models import Profile
 
-class UserUpdateForm(ModelForm):
+class UpdateNameForm(ModelForm):
     class Meta:
         model = User
-        exclude = ['id', 'kennitala', 'email', 'watchlist', 'auth_user_id']
+        exclude = ['id', 'email', 'username', 'password', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined', 'user_permissions', 'groups']
         widgets = {
             'first name': widgets.TextInput(attrs={'class': 'form-control'}),
             'last name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'phone_number': widgets.NumberInput(attrs={'class': 'form-control'}),
-            'address': widgets.TextInput(attrs={'class': 'form-control'}),
-            'postal_code': widgets.Select(attrs={'class': 'form-control'})
         }
 
 
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ['id', 'user']
+        exclude = ['id', 'user', 'kennitala']
         widgets = {
-            'profile_image': widgets.TextInput(attrs={'class': 'form-control'})
+            'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
+            'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'phone_number': widgets.NumberInput(attrs={'class': 'form-control'})
         }
 
--- user/models.py
+++ user/models.py
@@ -1,21 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User as auth_user
-
-
-class User(models.Model):
-    kennitala = models.IntegerField()
-    phone_number = models.IntegerField()
-    address = models.CharField(max_length=255)
-    postal_code = models.IntegerField()
-    watchlist = models.TextField(blank=True, null=True)
-    auth_user_id = models.ForeignKey(auth_user, on_delete=models.CASCADE)
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999)
+    kennitala = models.IntegerField()
+    phone_number = models.IntegerField()
+    address = models.CharField(max_length=255)
+    postal_code = models.IntegerField()
 
-
-class UserImage(models.Model):
-    image = models.CharField(max_length=999)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
