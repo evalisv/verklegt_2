174dfb7723273381ea306e7b6dbadaaebacf63bd
Eva Lísa <evalisv@gmail.com>
2019-05-11 11:14:35 +0200

    make offer is well on it's way but not complete. Changed a line in estate_details to make button active. Yet to be tested

--- offer/views.py
+++ offer/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from offer.forms.offer_forms import MakeOfferForm
+from estate.models import Estate
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>offer</h1>')
+
+def make_offer(request, id):
+    estate = get_object_or_404(Estate, pk=id)
+
+    if request.method == 'POST':
+        form =  MakeOfferForm(data=request.POST)
+        if form.is_valid():
+            offer = form.save(commit=False)
+
+    else:
+        print(2)
+    return render(request, 'form/make_offer.html',{
+        'form': form
+    })
