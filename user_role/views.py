a11f0cde86d59f8b21db8d4e892d64ef34424a7f
Árni Fannar <arnis18@ru.is>
2019-05-12 22:39:26 +0000

    Listi yfir fasteignasala.

--- user_role/views.py
+++ user_role/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from user_role.models import UserRole
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>user role</h1>')
+
+def view_agents(request):
+    context = {'agents': UserRole.objects.filter(role='admin')}
+    return render(request, 'agent/index.html', context)
+
+
