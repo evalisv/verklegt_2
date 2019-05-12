5fcb357216e838090a46e4563a418180a551e0e5
unknown <birtaosk93@gmail.com>
2019-05-12 03:05:17 +0200

    working in improving the search

--- searches/views.py
+++ searches/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import SearchQuery
 from estate.models import Estate
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def search_view(request):
@@ -12,7 +13,8 @@ def search_view(request):
     context = {'query': query}
     if query is not None:
         SearchQuery.objects.create(user=user, query=query)
-        estates = Estate.objects.all().filter(address__icontains=query)
+        lookup = (Q(address__icontains=query) | Q(description__icontains=query) | Q(estae__postal_code__contains=query))
+        estates = Estate.objects.all().filter(lookup)
 
         paginator = Paginator(estates, 6)
 
