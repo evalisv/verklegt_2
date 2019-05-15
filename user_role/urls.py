9a9c07fbc301b3131ce37dddcaeed1739e2b4eaf
Eva Lísa <evalisv@gmail.com>
2019-05-15 19:37:49 +0200

    Pending

--- user_role/urls.py
+++ user_role/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='userrole-index'),
     path('index/', views.view_agents, name='userrole-index'),
     path('index/register_agent/', views.register, name='register_agent')
-
+    # TODO: fix after functionality is finished
 ]
